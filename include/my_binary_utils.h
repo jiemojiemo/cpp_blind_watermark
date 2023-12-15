86f377a55ad5ce8ae8d923e52a9205a97abb7366
william <460706836@qq.com>
2023-12-15 16:05:43 +0800

    添加 my_binary_utils.h

---
+++ include/my_binary_utils.h
@@ -0,0 +1,52 @@
+//
+// Created by user on 12/15/23.
+//
+
+#ifndef BLINDWATERMARKTEST_MY_BINARY_UTILS_H
+#define BLINDWATERMARKTEST_MY_BINARY_UTILS_H
+
+#include "my_base64codec_utils.h"
+#include <bitset>
+#include <climits>
+#include <string>
+#include <vector>
+
+namespace my_lib {
+class BinaryUtils {
+public:
+  static std::vector<uint8_t> asciiStrToBinArray(const std::string &str) {
+    auto result = std::vector<uint8_t>();
+    for (const char &c : str) {
+      std::bitset<CHAR_BIT> b(c);
+      for (int i = CHAR_BIT - 1; i >= 0; --i) {
+        result.push_back(b[i]);
+      }
+    }
+    return result;
+  }
+
+  static std::string binArrayToAsciiStr(const std::vector<uint8_t> &bin_array) {
+    std::string ascii_str;
+    for (size_t i = 0; i < bin_array.size(); i += CHAR_BIT) {
+      std::bitset<CHAR_BIT> bits;
+      for (size_t j = 0; j < CHAR_BIT; ++j) {
+        bits.set(CHAR_BIT - 1 - j, bin_array[i + j]);
+      }
+      ascii_str.push_back(static_cast<char>(bits.to_ulong()));
+    }
+    return ascii_str;
+  }
+
+  static std::vector<uint8_t> utf8StrToBinArray(const std::string &str) {
+    auto base64_str = Base64CodecUtils::Encode(str);
+    return asciiStrToBinArray(base64_str);
+  }
+
+  static std::string binArrayToUtf8Str(const std::vector<uint8_t> &bin_array) {
+    auto ascii_str = binArrayToAsciiStr(bin_array);
+    return Base64CodecUtils::Decode(ascii_str);
+  }
+};
+} // namespace my_lib
+
+#endif // BLINDWATERMARKTEST_MY_BINARY_UTILS_H
---
+++ tests/test_binary_utils.cpp
@@ -0,0 +1,29 @@
+//
+// Created by user on 12/14/23.
+//
+
+#include <gmock/gmock.h>
+#include "my_binary_utils.h"
+
+using namespace testing;
+using namespace my_lib;
+
+class ABinaryUtils : public Test {
+public:
+};
+
+TEST_F(ABinaryUtils, CanTransASCIIStringToBinaryArray) {
+  auto test_str = "hello world";
+  auto array = BinaryUtils::asciiStrToBinArray(test_str);
+  auto str = BinaryUtils::binArrayToAsciiStr(array);
+
+  ASSERT_THAT(str, Eq(test_str));
+}
+
+TEST_F(ABinaryUtils, CanTransUTF8StringToBinaryArray) {
+  auto test_str = "hello world, 你好世界";
+  auto array = BinaryUtils::utf8StrToBinArray(test_str);
+  auto str = BinaryUtils::binArrayToUtf8Str(array);
+
+  ASSERT_THAT(str, Eq(test_str));
+}
